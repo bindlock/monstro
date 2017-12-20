692fe20b1f5d95298a2aefdc0f72aae1d38911d6
Vitalii Maslov <me@pyvim.com>
2017-12-20 17:37:46 +0300

    Not pass None value to document on save.

--- monstro/db/model.py
+++ monstro/db/model.py
@@ -139,6 +139,7 @@ class Model(object, metaclass=MetaModel):
                 await cls.Meta.collection.create_index(
                     ((name, field.index),),
                     unique=field.unique,
+                    sparse=True,
                     background=True
                 )
 
@@ -172,9 +173,7 @@ class Model(object, metaclass=MetaModel):
         for name, field in self.Meta.fields.items():
             value = self.Meta.data.get(name)
 
-            if value is None:
-                data[name] = value
-            else:
+            if value is not None:
                 data[name] = await field.db_serialize(value)
 
         return data
@@ -216,7 +215,7 @@ class Model(object, metaclass=MetaModel):
             await self.validate()
 
         data = await self.db_serialize()
-        data.pop('_id')
+        data.pop('_id', None)
 
         try:
             if self._id:
